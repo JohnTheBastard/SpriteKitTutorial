a4eb1ac1509c6c31fdec8f03a30aef01da82c48a
John Hearn <jhearn@gmail.com>
2016-10-20 21:00:55 -0700

    added collision detection between projectile and monster

--- SpriteKitTutorial/GameScene.swift
+++ SpriteKitTutorial/GameScene.swift
@@ -9,6 +9,13 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Monster   : UInt32 = 0b1
+    static let Projectile: UInt32 = 0b10
+}
+
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
@@ -42,7 +49,7 @@ extension CGPoint {
 }
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "player")
 
@@ -50,6 +57,9 @@ class GameScene: SKScene {
         backgroundColor = SKColor.white
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         addChild(player)
+
+        physicsWorld.gravity = CGVector.zero
+        physicsWorld.contactDelegate = self
 
         run(SKAction.repeatForever(
             SKAction.sequence([ SKAction.run(addMonster),
@@ -70,11 +80,25 @@ class GameScene: SKScene {
         let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
         monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
         addChild(monster)
+
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
+        monster.physicsBody?.isDynamic = true
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None
+
+
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(2.0))
         let actionMove = SKAction.move(to: CGPoint(x: -monster.size.width/2, y: actualY),
                                        duration: TimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
         monster.run(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+
+    func projectileDidCollideWithMonster(projectile: SKSpriteNode, monster: SKSpriteNode) {
+        print("Hit")
+        projectile.removeFromParent()
+        monster.removeFromParent()
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -83,6 +107,13 @@ class GameScene: SKScene {
 
         let projectile = SKSpriteNode(imageNamed: "projectile")
         projectile.position = player.position
+
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.isDynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
 
         let offset = touchLocation - projectile.position
         if (offset.x < 0) { return }
@@ -96,6 +127,24 @@ class GameScene: SKScene {
         let actionMove = SKAction.move(to: realDest, duration: 1.0)
         let actionMoveDone = SKAction.removeFromParent()
         projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
+    }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody:  SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody  = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody  = contact.bodyB
+            secondBody = contact.bodyA
+        }
+
+        if (( firstBody.categoryBitMask  & PhysicsCategory.Monster    != 0 ) &&
+            ( secondBody.categoryBitMask & PhysicsCategory.Projectile != 0 )) {
+            projectileDidCollideWithMonster(projectile: firstBody.node as! SKSpriteNode,
+                                            monster:   secondBody.node as! SKSpriteNode)
+        }
     }
 }
 
